ce9803633cf70dfbd64d9c4de0e154207eda6859
Anuj Kumar <47280522+Anuj-Kumar97@users.noreply.github.com>
2020-04-16 21:21:11 +0530

    Update

--- Spring_Data/src/main/java/com/iter/spring/utils/AdvertiseUtils.java
+++ Spring_Data/src/main/java/com/iter/spring/utils/AdvertiseUtils.java
@@ -1,6 +1,7 @@
 package com.iter.spring.utils;
 
 import java.util.ArrayList;
+
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -9,25 +10,24 @@ import com.iter.spring.json.Advertise;
 
 public class AdvertiseUtils {
 	
-	public static List<Advertise> convertAdvEntityListToAdvJson(List<AdvertiseEntity> adEntityList)
+	public static List<Advertise> convertAdEntityListToAdJson(List<AdvertiseEntity> adEntityList)
 	{
 		List<Advertise> adList=new ArrayList<Advertise>();
-		Consumer<AdvertiseEntity> consumer=(AdvertiseEntity ad)->adList.add(convertAdvEntityToAdvJson(ad));
+		Consumer<AdvertiseEntity> consumer=(AdvertiseEntity ad)->adList.add(convertAdEntityToAdJson(ad));
 		adEntityList.stream().forEach(consumer);
 		return adList;
 	}
 	
-	public static Advertise convertAdvEntityToAdvJson(AdvertiseEntity adEntity)
+	public static Advertise convertAdEntityToAdJson(AdvertiseEntity adEntity)
 	{
 		return new Advertise(adEntity.getId(),adEntity.getName(),adEntity.getTitle(),adEntity.getCategory(),
-				adEntity.getDescription(),adEntity.getPostID(),adEntity.getStatus(),
-				UserUtils.convertUserEntityToUser(adEntity.getUserEntity()));
+				adEntity.getDescription(),adEntity.getPostID(),adEntity.getStatus(),adEntity.getLdt());
 	}
-	public static AdvertiseEntity convertAdvJsonToAdvEntity(Advertise adJson)
+	
+	public static AdvertiseEntity convertAdJsonToAdEntity(Advertise adJson)
 	{
 		return new AdvertiseEntity(adJson.getId(),adJson.getName(),adJson.getTitle(),adJson.getCategory(),
-				adJson.getDescription(),adJson.getPostID(),adJson.getStatus(),
-				UserUtils.convertUserToUserEntity(adJson.getUser()));
+				adJson.getDescription(),adJson.getPostID(),adJson.getStatus(),adJson.getLdt());
 	}
 
 }
